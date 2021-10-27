3b231922f37a34e9d2297f5dac95817ab1a1c12e
Екатерина Язовских <jaz-k@yandex.ru>
2021-10-27 14:33:07 +0500

    выполнено ДЗ 1

---
+++ Задание 1 стих/Задание 1 стих/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задание_1_стих
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Мы не жалеем времени...");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Мы не жалеем времени,");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("А время наше нас,");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Летит, не зная стремени,");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Как с крыльями Пегас.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+            }
+        }
+    
+}
