994af5db9dcb9e4ce0f95f8415d4f8866e3b6d00
Екатерина Язовских <jaz-k@yandex.ru>
2021-10-27 15:32:07 +0500

    задание 2 выполнено

---
+++ задание №2 Додекаэдр/задание №2 Додекаэдр/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace задание__2_Додекаэдр
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double V, S;
+            Console.WriteLine("Введите длину стороны правильного додекаэдра");
+            var a = double.Parse(Console.ReadLine());
+            S = 3 * a * a * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5)));
+            V = (Math.Pow(a, 3) * (15 + 7 * Math.Sqrt(5))) / 4;
+            Console.WriteLine($"Площадь поверхности додекаэдра ={S}");
+            Console.WriteLine($"Объем додекаэдра ={V}");
+        }
+    }
+}
